690c7b60be9f0cf0412899adbad6c64a5da0bd70
Steve Polito <info@stevepolitodesign.com>
2020-05-13 15:21:38 -0400

    Wite simple test for base class.

--- test/simple_note_parser_base_test.rb
+++ test/simple_note_parser_base_test.rb
@@ -1,8 +1,24 @@
 require "test_helper"
 require "simple_note_parser/base"
 
-class SimpleNoteParser::BaseTest < Minitest::Test    
+class SimpleNoteParser::BaseTest < Minitest::Test
+    def setup
+        @notes = SimpleNoteParser::Base.new
+    end
+
     def test_that_it_can_be_instantiated
-        @notes = SimpleNoteParser::Base.new()
+        assert @notes
+    end
+
+    def test_that_it_has_a_default_file_value
+        assert_equal "./source/notes.json", @notes.file
+    end
+
+    def test_that_it_has_a_default_destination_value
+        assert_equal "./dist", @notes.destination
+    end
+
+    def test_that_it_reponds_to_clean
+        assert_respond_to @notes, :clean
     end
 end
